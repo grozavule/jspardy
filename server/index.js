c4d30b6e7a09806446e63e1499c494e31612bbb4
Eric Drake <puturosule@gmail.com>
2022-12-21 12:23:18 -0700

    created the registration form UI

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 
 const DatabaseMigrationController = require('./controllers/DatabaseMigrationController');
 
-const {BASE_URL, SERVER_PORT} = process.env;
+const {SERVER_PORT} = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(express.static('client'));
 app.use(cors());
 
+app.get('/', (req, res) => res.sendFile('index.html'));
 app.get('/db-migrate', DatabaseMigrationController.populate);
 
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
